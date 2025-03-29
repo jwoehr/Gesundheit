4bc1d277cb477f3cea7dc2447f7b851b9d7bc4c1
jwoehr <jwoehr@softwoehr.com>
2025-03-29 09:35:15 -0600

    - named params - added UserModelTestData

--- test/UserModelTest.php
+++ test/UserModelTest.php
@@ -26,6 +26,8 @@
 
 use PHPUnit\Framework\TestCase;
 
+require_once __DIR__ . '/data/UserModelTestData.php';
+
 class UserModelTest extends TestCase {
 
     private DbModel $dbmodel;
@@ -38,31 +40,31 @@ class UserModelTest extends TestCase {
         $this->dbmodel->connect();
     }
 
-    public function testUserModel() {
+    public function testUserModel(): void {
         $m = new UserModel();
-        print($m);
-        $m->setUsernum(1);
-        $m->setName('jax');
-        $m->setPassword('arfarf');
-        print($m);
-        $m->save($this->dbmodel);
+        print ($m);
+        $m->setUsernum(usernum: 1);
+        $m->setName(name: 'jax');
+        $m->setPassword(password: 'arfarf');
+        print ($m);
+        $m->save(dbmodel: $this->dbmodel);
         $m1 = new UserModel();
         $m1->load(dbmodel: $this->dbmodel, usernum: 1);
-        print($m1);
-        $m1->setName("fred");
-        $m1->setPassword("woofwoof");
-        $m1->save($this->dbmodel);
+        print ($m1);
+        $m1->setName(name: "fred");
+        $m1->setPassword(password: "woofwoof");
+        $m1->save(dbmodel: $this->dbmodel);
         $m->load(dbmodel: $this->dbmodel, usernum: 1);
-        print($m);
+        print ($m);
         $m = new UserModel();
-        $m->setUsernum(2);
-        $m->setName('sumi');
-        $m->setPassword('gravel');
-        $m->save($this->dbmodel);
+        $m->setUsernum(usernum: 2);
+        $m->setName(name: 'sumi');
+        $m->setPassword(password: 'gravel');
+        $m->save(dbmodel: $this->dbmodel);
         $m1 = new UserModel();
-        $m1->load($this->dbmodel, usernum: 2);
-        print($m1);
-        $this->expectOutputString('UserModel[usernum=0, name=, password=]UserModel[usernum=1, name=jax, password=arfarf]UserModel[usernum=1, name=jax, password=arfarf]UserModel[usernum=1, name=fred, password=woofwoof]UserModel[usernum=2, name=sumi, password=gravel]');
+        $m1->load(dbmodel: $this->dbmodel, usernum: 2);
+        print ($m1);
+        $this->expectOutputString(expectedString: UserModelTestData::TEMP_STR);
     }
 
     protected function tearDown(): void {
