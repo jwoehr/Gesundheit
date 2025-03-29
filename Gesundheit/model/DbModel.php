01d6a02bf4f30affbe6381da4bc533618106bea6
jwoehr <jwoehr@softwoehr.com>
2025-03-28 18:00:09 -0600

    Added require of vendor

--- Gesundheit/model/DbModel.php
+++ Gesundheit/model/DbModel.php
@@ -23,6 +23,8 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+
+require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/../util/Util.php';
 
 /**
